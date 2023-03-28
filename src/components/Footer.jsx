dc3b1d99e2d7fe5312f0e4234e0af49b15a567a5
Supravat Dwari <86393808+SupravatDwari@users.noreply.github.com>
2023-03-28 22:50:35 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
       <Box sx={{ mt: 'auto', p: '0.2rem', bgcolor: '#efefef', zIndex:1201 }}>
         <Typography sx={{ textAlign: 'center', verticalAlign: 'middle' }}>
-          Made with love by <a style={{textDecoration: 'none'}} target="_blank" href="https://www.github.com/Sudhanva-Nadiger">Sudhanva</a>
+          Made with love by Supravat</a>
         </Typography>
       </Box>
   )
